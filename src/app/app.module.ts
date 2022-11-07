ef718db4d7ea79f7a1e0d6b958d4061a50a3c6fe
Erick <estudodoerick@gmail.com>
2022-11-07 12:23:23 -0300

    error sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
-import { TableComponent } from './components/table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  declarations: [AppComponent, RegistrationComponent, TableComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, RegistrationComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,21 +1,64 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { __values } from 'tslib';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface UserData {
+  name: string;
+  phone: number;
+}
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent implements OnInit {
-  constructor() {}
+export class RegistrationComponent implements AfterViewInit {
+  form!: FormGroup;
+
+  displayedColumns: string[] = ['name', 'phone'];
+
+  dataSource: UserData[] = [];
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private liveAnnouncer: LiveAnnouncer) {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      phone: new FormControl(null, Validators.required),
+    });
+
+    this.dataSource = [];
+  }
 
   ngOnInit(): void {}
 
-  click = document.getElementsByTagName('input');
+  clear(): void {
+    this.form.reset();
+  }
 
-  
+  saveData(): void {
+    const name = this.form.get('name')?.value;
+    const phone = this.form.get('phone')?.value;
 
-  clear() {
-    console.log(this.click);
+    this.dataSource.push({ name, phone });
+
+    this.dataSource = [...this.dataSource];
+
+    this.clear();
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this.liveAnnouncer.announce(`Sorted ${sortState.direction} final`);
+    } else {
+      this.liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
   }
 }
